ea939e3e5cf847a3aa7994d5ce5fc8c1ab7e4c06
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-01-09 18:57:22 +0100

    Aggiunta una classe movie nella quale sono state dichiarate variabili d'istanza e definito un costruttore

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php 
-    
+    class Movie{
+        public $title;
+        public $genre; 
+        public $year;
+        public $duration;
+        public $rating;
+        public $language;
+
+        function __construct($_title, $_genre, $_year, $_duration, $_rating, $_language)
+        {
+            $this->title = $_title;
+            $this->genre = $_genre;
+            $this->year = $_year;
+            $this->duration = $_duration;
+            $this->rating = $_rating;
+            $this->language = $_language;
+        }
+    }
 
 ?>
