63769240fd9b47a92936661f99045b82f1d80f7c
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-01-09 22:49:37 +0100

    Aggiunto metodo alla classe

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
         public $duration;
         public $rating;
         public $language;
+        public $price;
+        public $discount = 0;
 
-        function __construct($_title, $_genre, $_year, $_duration, $_rating, $_language)
+        function __construct($_title, $_genre, $_year, $_duration, $_rating, $_language, $_price)
         {
             $this->title = $_title;
             $this->genre = $_genre;
@@ -15,7 +17,19 @@
             $this->duration = $_duration;
             $this->rating = $_rating;
             $this->language = $_language;
+            $this->price = $_price;
+            $this->setDiscount($_year);
         }
+
+        public function setDiscount($year) {
+            if($year < 1972) {
+                $this->discount = $this->price * 0.2;
+            } 
+        }
+        public function getDiscount() {
+            return $this->discount;
+      
+          }
     }
 
 ?>
